7bcc6f8beb868c4f2ee36f0bcc8ef745becb795d
JSurey <meza.201120606@gmail.com>
2025-06-12 23:54:54 -0600

    change name

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let page = document.querySelector(".page");
+let popup = document.querySelector(".popup");
+let content = page.querySelector(".content");
+let profile = content.querySelector(".profile");
+let profileEditButton = profile.querySelector(".profile__edit-button");
+let closePopupButton = popup.querySelector(".popup__close-button");
+let formElement = popup.querySelector(".popup__container");
